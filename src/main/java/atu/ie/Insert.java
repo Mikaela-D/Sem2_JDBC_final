f477967401aab775a7da0eb0406e42e97888d7b5
Mikaela-D <G00401392@atu.ie>
2023-04-04 22:47:22 +0100

    Added a try catch with data to be inserted in the database

--- src/main/java/atu/ie/Insert.java
+++ src/main/java/atu/ie/Insert.java
@@ -2,12 +2,31 @@ package atu.ie;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Insert {
     public static void main(String[] args) throws SQLException {
 
         // Connect to the database
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/exampledatabase", "Mikaela Diaz", "someSadPassword...883");
+        Connection con = DriverManager.getConnection("C:\\orangeBackup", "Mikaela Diaz", "someSadPassword...883");
+
+        try {
+            // Insert a new record into the "users" table
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO person (id, name, age, email) VALUES (?, ?, ?, ?)");
+            stmt.setString(1, "11");
+            stmt.setString(2, "Myz");
+            stmt.setString(3, "48");
+            stmt.setString(4, "myz@example.com");
+            stmt.executeUpdate();
+
+            System.out.println("Insert completed successfully.");
+        } catch (SQLException ex) {
+
+            System.out.println("Record insert failed.");
+            ex.printStackTrace();
+        }
+        // Close the connection
+        con.close();
     }
 }
