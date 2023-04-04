b5fda9bbedf54b685fe31de97c2284bcf417b0f8
Mikaela-D <G00401392@atu.ie>
2023-04-04 22:28:26 +0100

    Added code to connect to the database

--- src/main/java/atu/ie/Insert.java
+++ src/main/java/atu/ie/Insert.java
@@ -1,4 +1,13 @@
 package atu.ie;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Insert {
+    public static void main(String[] args) throws SQLException {
+
+        // Connect to the database
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/exampledatabase", "Mikaela Diaz", "someSadPassword...883");
+    }
 }
