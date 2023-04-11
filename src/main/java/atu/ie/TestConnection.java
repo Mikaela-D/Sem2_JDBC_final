1fba6e941d753bb7cfffd5945466d00d6930195c
Mikaela-D <G00401392@atu.ie>
2023-04-11 14:34:53 +0100

    Added variables

--- src/main/java/atu/ie/TestConnection.java
+++ src/main/java/atu/ie/TestConnection.java
@@ -1,4 +1,10 @@
 package atu.ie;
 
 public class TestConnection {
+    public static void main(String[] args) {
+        // MySQL database connection details
+        String url = "jdbc:mysql://localhost:3306/orange";
+        String username = "root";
+        String password = "someSadPassword...883";
+    }
 }
