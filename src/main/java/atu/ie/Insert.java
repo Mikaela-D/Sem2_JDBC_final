7e55f23a759b8e4850aefcac051852757f02b3e7
Mikaela-D <G00401392@atu.ie>
2023-04-11 14:28:39 +0100

    Changed username again

--- src/main/java/atu/ie/Insert.java
+++ src/main/java/atu/ie/Insert.java
@@ -9,7 +9,7 @@ public class Insert {
     public static void main(String[] args) throws SQLException {
 
         // Connect to the database
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/orange", "MD", "someSadPassword...883");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/orange", "root", "someSadPassword...883");
 
         try {
             // Insert a new record into the "users" table
