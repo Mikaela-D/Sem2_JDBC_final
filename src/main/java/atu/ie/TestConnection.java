5b860cf402cae4ca7681049a8360c65d3d42b176
Mikaela-D <G00401392@atu.ie>
2023-04-11 14:45:13 +0100

    Added try catch to the code

--- src/main/java/atu/ie/TestConnection.java
+++ src/main/java/atu/ie/TestConnection.java
@@ -1,5 +1,7 @@
 package atu.ie;
 
+import java.sql.*;
+
 public class TestConnection {
     public static void main(String[] args) {
         // MySQL database connection details
@@ -12,5 +14,19 @@ public class TestConnection {
                 "FROM users u " +
                 "JOIN emails e ON u.id = e.user_id";
 
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectSQL)) {
+
+            while (resultSet.next()) {
+                String user = resultSet.getString("username");
+                String pass = resultSet.getString("password");
+                String email = resultSet.getString("email");
+
+                System.out.println("Username: " + user + ", Password: " + pass + ", Email: " + email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
